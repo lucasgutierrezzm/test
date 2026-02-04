df6c737939a73fa989f5e623c19589729726738d
lucasgutierrezzm <lucas.guitierrez@sellside.cl>
2026-02-03 21:49:48 -0300

    Update mail_message.py
    Se cambia el mail_message para que no acepte todo

--- ai_inventory_crm/models/mail_message.py
+++ ai_inventory_crm/models/mail_message.py
@@ -1,4 +1,5 @@
 from odoo import models, api
+from odoo.tools import html2plaintext
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -11,21 +12,29 @@ class MailMessage(models.Model):
         message = super().create(vals)
 
         try:
+            # Solo mensajes humanos en canales de Discuss
             if (
-                message.model == 'discuss.channel'
-                and message.body
-                and message.author_id
+                message.model != 'discuss.channel'
+                or not message.body
+                or message.author_id.user_ids
+                and message.author_id.user_ids[0].login == '__odoobot__'
             ):
-                _logger.warning(
-                    "AI MODULE: mail.message intercepted: %s",
-                    message.body
-                )
+                return message
 
-                channel = self.env['discuss.channel'].browse(message.res_id)
-                self.env['ai.inventory.crm.logic'].process_message(
-                    channel=channel,
-                    text=message.body
-                )
+            text = html2plaintext(message.body).strip()
+
+            if not text:
+                return message
+
+            _logger.warning("AI MODULE: clean message = %s", text)
+
+            channel = self.env['discuss.channel'].browse(message.res_id)
+
+            self.env['ai.inventory.crm.logic'].process_message(
+                channel=channel,
+                text=text
+            )
+
         except Exception as e:
             _logger.error("AI MODULE ERROR: %s", e)
 
